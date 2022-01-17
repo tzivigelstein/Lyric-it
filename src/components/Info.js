a3c9143d6da510143b4831797321b4c2779cfacd
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-01-17 15:09:19 -0300

    Update Info component data

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,29 +1,18 @@
 import React from 'react'
 
-const Info = ({ descriptions }) => {
+const Info = ({ artistData }) => {
   //Extrayendo valores
 
-  const { strArtist, strArtistThumb, strGenre, strBiographyES, strLastFMChart, strTwitter, strFacebook } = descriptions
+  const { artist, thumbnail, genre, biography } = artistData
 
   return (
     <div className="card border-light">
-      <div className="card-header bg-primary text-light font-weight-bold">{strArtist}</div>
+      <div className="card-header bg-primary text-light font-weight-bold">{artist}</div>
       <div className="card-body">
-        <img src={strArtistThumb} alt="Band Logo" />
-        <p className="card-text">{strGenre}</p>
+        <img src={thumbnail} alt="Band Logo" />
+        <p className="card-text">{genre}</p>
         <h2 className="card-text">Biography</h2>
-        <p className="card-text">{strBiographyES}</p>
-        <p className="card-text">
-          <a href={`https://${strFacebook}`} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href={`https://${strTwitter}`} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href={`${strLastFMChart}`} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-lastfm"></i>
-          </a>
-        </p>
+        <p className="card-text">{biography}</p>
       </div>
     </div>
   )
